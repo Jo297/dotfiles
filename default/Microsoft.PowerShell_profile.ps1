f638129e7b9830319b02412cfdca96c9eb6348e1
Jo297 <39516057+Jo297@users.noreply.github.com>
2022-04-08 20:36:55 +0530

    ➕ add files
    powershell customization

--- default/Microsoft.PowerShell_profile.ps1
+++ default/Microsoft.PowerShell_profile.ps1
@@ -1,10 +1,18 @@
 #Documents/WindowsPowershell
 
+
+
 #Prompt
 Import-Module posh-git
 Import-Module oh-my-posh
 #Set-PoshPrompt -Theme Paradox
-Set-PoshPrompt -Theme atomic
+#Set-PoshPrompt -Theme atomic
+
+#Load prompt config
+function Get-ScriptDirectory { Split-Path $MyInvocation.ScriptName }
+$PROMPT_CONFIG = Join-Path (Get-ScriptDirectory) 'joDevice.omp.json'
+
+oh-my-posh --init --shell pwsh --config $PROMPT_CONFIG | Invoke-Expression
 
 Import-Module -Name Terminal-Icons
 
